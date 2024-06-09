00672c5b7fe0cfd833e72e301661be7e3803e3bb
Collins1Kibet <collins1kibet@gmail.com>
2024-06-09 17:01:37 +0300

    Update Sentiment_Analysis_App.py

--- Sentiment_Analysis_App.py
+++ Sentiment_Analysis_App.py
@@ -7,24 +7,20 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 working_directory = os.path.dirname(os.path.abspath(__file__))
-
 model_path = os.path.join(working_directory, "Notebook", "Movie_Reviews_Model.h5")
 tokenizer_path = os.path.join(working_directory, "Notebook", "tokenizer_word_index.json")
 
 model = load_model(model_path)
 
-tokenizer = Tokenizer(num_words=5000)
-tokenizer.fit_on_texts('review')
-sequences = tokenizer.texts_to_sequences('review')
-
 with open(tokenizer_path, 'r') as json_file:
     word_index = json.load(json_file)
-if tokenizer:
-    tokenizer.word_index = word_index
+
+tokenizer = Tokenizer(num_words=5000)
+tokenizer.word_index = word_index
 
 def predict_sentiment(review):
     sequence = tokenizer.texts_to_sequences([review])
-    padded_sequence = pad_sequences(sequence, maxlen= 200)
+    padded_sequence = pad_sequences(sequence, maxlen=200)
     prediction = model.predict(padded_sequence)
     sentiment = 'Positive' if prediction[0][0] > 0.5 else 'Negative'
     return sentiment
@@ -35,5 +31,8 @@ st.write('Enter a Movie Review to predict its sentiment (Positive/Negative).')
 input_review = st.text_area('Enter the review...')
 
 if st.button('Predict Sentiment'):
-    sentiment = predict_sentiment(input_review)
-    st.write('The Sentiment of the Review is: ', sentiment)
+    if input_review.strip():
+        sentiment = predict_sentiment(input_review)
+        st.write('The Sentiment of the Review is: ', sentiment)
+    else:
+        st.write('Please enter a valid review.')
