a672f40f53fa1adc3ecb9460b2538877af788d03
Collins1Kibet <collins1kibet@gmail.com>
2024-06-09 12:29:50 +0300

    Update Sentiment_Analysis_App.py

--- Sentiment_Analysis_App.py
+++ Sentiment_Analysis_App.py
@@ -1,23 +1,24 @@
+import os
 import json
 import numpy as np
 import streamlit as st
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequense import pad_sequences
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 working_directory = os.path.dirname(os.path.abspath(__file__))
 movie_reviews_model_path = os.path.join(working_directory, 'Notebook', 'Movie_Reviews_Model.h5')
-movie_reviews_tokenizer = os.path.join(working_directory, 'Notebook', 'tokenizer.json')
+movie_reviews_tokenizer_path = os.path.join(working_directory, 'Notebook', 'tokenizer.json')  # Corrected variable name
 
 model = load_model(movie_reviews_model_path)
 
-with open(movie_reviews_tokenizer, 'r') as t:
+with open(movie_reviews_tokenizer_path, 'r') as t:
     tokenizer_data = json.load(t)
     tokenizer = Tokenizer.from_json(tokenizer_data)
 
 def predict_sentiment(review):
-    sequence = tokenizer.text_to_sequence([review])
-    padded_sequence = pad_sequences(sequence, maxlen= 200)
+    sequence = tokenizer.texts_to_sequences([review])
+    padded_sequence = pad_sequences(sequence, maxlen=200)
     prediction = model.predict(padded_sequence)
     sentiment = 'Positive' if prediction[0][0] > 0.5 else 'Negative'
     return sentiment
